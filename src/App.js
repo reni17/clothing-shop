a1489addd49fbd9a272044698fabdc31cc6dc670
reni17 <69053305+reni17@users.noreply.github.com>
2022-09-14 16:25:27 +0300

    add home page routes

--- src/App.js
+++ src/App.js
@@ -63,33 +63,6 @@ function App() {
     setCartItems(addCartItem(cartItems, productToAdd))
   }
 
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
 
   return (
     <UserContext.Provider value={{user, setUser}}>
@@ -100,7 +73,7 @@ function App() {
               <Route
                 path="/"
                 index
-                element={<Home categories={categories}></Home>}
+                element={<Home></Home>}
               ></Route>
               <Route
                 path="shop/*"
--- src/components/directory/DirectoryItem.js
+++ src/components/directory/DirectoryItem.js
@@ -1,11 +1,16 @@
 import './DirectoryItem.styles.js'
+import { useNavigate } from 'react-router-dom'
 import { BackgroundImage, Body, DirectoryItemContainer } from './DirectoryItem.styles.js'
-const DirectoryItem = ({id, imageUrl, title}) => {
+const DirectoryItem = ({id, imageUrl, title, route}) => {
+const navigate = useNavigate()
+const NavigateHandler = () => {
+navigate(route)
+}
 
     return (
         <DirectoryItemContainer key={id} >
         <BackgroundImage imageUrl={imageUrl} ></BackgroundImage>
-        <Body>
+        <Body onClick={NavigateHandler}>
           <h2>{title}</h2>
           <p>Shop now</p>
         </Body>
